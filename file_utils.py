2e934ba72a8d66fcdfab833bb3b8963bfd4aeff1
bucks-hub <shaheembacker@icloud.com>
2025-05-23 10:42:14 +0200

    Delete file_utils.py

--- file_utils.py
+++
@@ -1,7 +0,0 @@
-import cv2
-import os
-from config import SAVE_PATH
-
-def save_frame(frame, frame_count):
-    filename = os.path.join(SAVE_PATH, f"frame_{frame_count}.jpg")
-    cv2.imwrite(filename, frame)
