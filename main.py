4b891736fcef81f2edd97188f86aadeea09c0798
bucks-hub <shaheembacker@icloud.com>
2025-06-03 15:16:20 +0200

    Update main.py
    Used multiprocessing package for cable detection accross multiple cameras.

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
-from hacadet_pkg import *
+from hacadet import *
+import multiprocessing as mp
 
 
-def main():
+def main(n):
     # Initialize components
     bmw_model = YOLO(BMW_MODEL_PATH)
     sound = SoundManager()
-    capture = get_capture()
+    capture = get_capture(n)
     frame_count = 0
     paused = False
     terminated = False
@@ -14,20 +15,21 @@ def main():
         if not paused:
             while capture.isOpened():
                 success, frame = capture.read()
+                # print(frame.shape)
                 if not success:
                     print("Reconnecting...")
-                    capture = get_capture()
+                    capture = get_capture(n)
                     continue
                 width = frame.shape[1]
-                frame = resize_frame(frame)
+                # frame = resize_frame(frame)
 
                 if frame_count % FRAME_SKIP == 0:
                     # Process frame
                     cables, skids, frame = detect_objects(frame, bmw_model)
                     alert = check_alert(cables, skids, THRESHOLD, width)
-                    cv.namedWindow("HaCaDet", cv.WINDOW_NORMAL)
-                    cv.setWindowProperty("HaCaDet", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
-                    cv.imshow("HaCaDet", frame)
+                    cv.namedWindow(f"HaCaDet - Camera {n}", cv.WINDOW_NORMAL)
+                    # cv.setWindowProperty(f"HaCaDet - Camera {n}", cv.WND_PROP_FULLSCREEN, cv.WINDOW_FULLSCREEN)
+                    cv.imshow(f"HaCaDet - Camera {n}", frame)
                     # Handle alerts
                     if alert:
                         save_frame(frame, frame_count)
@@ -35,9 +37,13 @@ def main():
                         sound.play(alert)
                         print("Press 'p' to mute")
                         while True:
-                            key = cv.waitKey(0)
+                            cv.setWindowProperty(f"HaCaDet - Camera {n}", cv.WND_PROP_TOPMOST, 3)
+                            cv.imshow(f"HaCaDet - Camera {n}", frame)
+                            key = cv.waitKey(5000)
                             if key == ord('p'):
                                 sound.stop(alert)
+                                cv.namedWindow("HaCaDet-Fix the issue!!!", cv.WINDOW_NORMAL)
+                                cv.setWindowProperty("HaCaDet-Fix the issue!!!", cv.WND_PROP_TOPMOST, 1)
                                 cv.imshow("HaCaDet-Fix the issue!!!", frame)
                                 print("Fix the issue; press 'p' to resume; 'Esc' to terminate")
                                 key = cv.waitKey(0)
@@ -47,6 +53,10 @@ def main():
                                 elif key == ord('p'):
                                     cv.destroyWindow("HaCaDet-Fix the issue!!!")
                                     break
+                            else:
+                                sound.stop(alert)
+                                print("Time Out")
+                                break
                         if terminated:
                             break
                     else:
@@ -80,4 +90,11 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    print("\nProject HaCaDet: No more low hanging cables!!!")
+    for i in range(len(CAMERA_URL)):
+        mp.Process(target=main, args=[i]).start()
+
+    children = mp.active_children()
+
+    for child in children:
+        child.join()
